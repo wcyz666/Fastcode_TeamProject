e53fc0917ee098dd949cc702a8e65ac0a73279e8
WANG Cheng <wcyz666@126.com>
2016-04-16 17:13:15 -0700

    Optimized

--- src/itemcountSON/Driver.java
+++ src/itemcountSON/Driver.java
@@ -3,7 +3,7 @@ package itemcountSON;
 import job.Optimizedjob;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 import util.SimpleParser;
 
 import java.io.IOException;
@@ -25,9 +25,10 @@ public class Driver {
             throws IOException, ClassNotFoundException, InterruptedException {
         Optimizedjob job = new Optimizedjob(new Configuration(), input, output,
                 "Compute NGram Count using SON");
-        job.setMapJobs(15);
-        job.setClasses(ItemCountSONPass2Mapper.class, ItemCountSONPass2Reducer.class, ItemCountSONPass2Reducer.class);
-        job.setMapOutputClasses(Text.class, IntWritable.class);
+        //job.setMapJobs(15);
+        job.setClasses(ItemCountSONMapper.class, ItemCountSONReducer.class, ItemCountSONReducer.class);
+        //job.setClasses(ItemCountSONPass2Mapper.class, ItemCountSONPass2Reducer.class, ItemCountSONPass2Reducer.class);
+        job.setMapOutputClasses(IntWritable.class, NullWritable.class);
         job.setParameter("n", N);
         job.setParameter("pass1File", pass1File);
         job.setParameter("mapreduce.map.memory.mb", "6000");
